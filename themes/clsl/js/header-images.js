c828a0546784ef485eaea9814ef80d2ddb6cb3c8
Chris Morris <cmorris@ccistudios.com>
2017-09-27 14:46:10 -0400

    style the header, footer, homepage for desktop

---
+++ themes/clsl/js/header-images.js
@@ -0,0 +1,30 @@
+$(function(){
+  var paused = false;
+  var carousel;
+  
+  $(function(){
+    initCarousel();
+  });
+  function initCarousel(){
+    $(".view-header-images").append("<div class='slick-dots-container'></div>")
+    carousel = $(".view-header-images .view-content").slick({
+      arrows: true,
+      dots: true,
+      slidesToShow: 1,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      appendDots: ".slick-dots-container"
+    });
+    $(".view-header-images .slick-dots-container").prepend("<button type='button' class='slick-pause' title='Pause slideshow'>Pause slideshow</button>");
+    $(".view-header-images .slick-pause").on("click", pauseClick);
+  }
+  function pauseClick(e) {
+    $(e.target).toggleClass("is-paused");
+    if (paused) {
+      carousel.slick("slickPlay");
+    } else {
+      carousel.slick("slickPause");
+    }
+    paused = !paused;
+  }
+});
