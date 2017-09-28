174c5244744f753fb52a409af1efabccc135accf
Chris Morris <cmorris@ccistudios.com>
2017-09-28 13:38:40 -0400

    style basic pages and added fun facts block

---
+++ themes/clsl/js/fun-facts.js
@@ -0,0 +1,11 @@
+$(function(){  
+  $(function(){
+    var $rows = $(".view-fun-fact .views-row");
+    if ($rows.length) {
+      var min = 0;
+      var max = $rows.length;
+      var i = Math.floor(Math.random() * (max - min)) + min;
+      $($rows.get(i)).show();
+    }
+  });
+});
