1e145d907d86f81ab08d9f9d9f8d7b2459570795
Chris Morris <cmorris@ccistudios.com>
2017-09-29 13:57:14 -0400

    drush alias for staging site

---
+++ sites/all/drush/aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+
+$aliases['staging'] = array(
+	'uri'=> 'staging.communitylivingsarnia.org',
+	'root' => '/home/clsl/subdomains/staging/public_html',
+	'remote-host'=> 'host.cciserver2.com',
+	'remote-user'=> 'clsl',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	),
+);
