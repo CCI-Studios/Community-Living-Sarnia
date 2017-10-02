87fd2ed9dac9594f9cc62c3d3c88d7eb68cee1a6
Chris Morris <cmorris@ccistudios.com>
2017-10-02 12:19:33 -0400

    fix js components not working with a logged in admin close #1

--- themes/clsl/js/collapsible-content.js
+++ themes/clsl/js/collapsible-content.js
@@ -1,6 +1,8 @@
-$(function(){
-  $(document).on("click", ".paragraph--type--collapsible-content .field--name-field-title a", function(){
-    $(this).closest(".paragraph").toggleClass("open").find(".field--name-field-body").slideToggle();
-    return false;
+(function($){
+  $(function(){
+    $(document).on("click", ".paragraph--type--collapsible-content .field--name-field-title a", function(){
+      $(this).closest(".paragraph").toggleClass("open").find(".field--name-field-body").slideToggle();
+      return false;
+    });
   });
-});
+})(jQuery);
--- themes/clsl/js/fun-facts.js
+++ themes/clsl/js/fun-facts.js
@@ -1,11 +1,13 @@
-$(function(){  
-  $(function(){
-    var $rows = $(".view-fun-fact .views-row");
-    if ($rows.length) {
-      var min = 0;
-      var max = $rows.length;
-      var i = Math.floor(Math.random() * (max - min)) + min;
-      $($rows.get(i)).show();
-    }
+(function($){
+  $(function(){  
+    $(function(){
+      var $rows = $(".view-fun-fact .views-row");
+      if ($rows.length) {
+        var min = 0;
+        var max = $rows.length;
+        var i = Math.floor(Math.random() * (max - min)) + min;
+        $($rows.get(i)).show();
+      }
+    });
   });
-});
+})(jQuery);
